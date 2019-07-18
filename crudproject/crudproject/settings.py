f98e58d9ccad2cf5d4455c218e3d9e0120a7174e
sooahkim <saligo96@gmail.com>
2019-07-19 02:14:54 +0900

    4주차 과제 배포

--- crudproject/crudproject/settings.py
+++ crudproject/crudproject/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zul0$2sgzf^pqoo^7(uhy+$*+gwe$mx+p+-kgwa=$wli(smz1+'
-
+#SECRET_KEY = 'zul0$2sgzf^pqoo^7(uhy+$*+gwe$mx+p+-kgwa=$wli(smz1+'
+SECRET_KEY=os.environ.get('DJANGO_SECRET_KEY','zul0$2sgzf^pqoo^7(uhy+$*+gwe$mx+p+-kgwa=$wli(smz1+')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG=bool(os.environ.get('DJANGO_DEBUG',True))
 
 ALLOWED_HOSTS = []
 
